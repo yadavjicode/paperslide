28330a78f80180179f9bcf7adcfaf65e4aeb7ff8
yadavprashant123 <prashant.yadav@ajath.com>
2023-11-06 18:48:07 +0530

    scan and presentation

---
+++ app/src/main/java/com/example/paper_slide/Signature/Signature.kt
@@ -0,0 +1,12 @@
+package com.example.paper_slide.Signature
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.paper_slide.R
+
+class Signature : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signature)
+    }
+}
