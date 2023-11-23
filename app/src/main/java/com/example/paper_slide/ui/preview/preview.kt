fbfeba1d55c1fccb2848b79186f9a4ec72d11ff2
ChetanSinghNegiCSE <chetansinghnegicse@gmail.com>
2023-11-23 18:35:59 +0530

    preview MVVM and Data Binding

--- app/src/main/java/com/example/paper_slide/ui/preview/preview.kt
+++ app/src/main/java/com/example/paper_slide/ui/preview/preview.kt
@@ -2,19 +2,27 @@ package com.example.paper_slide.ui.preview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager.widget.ViewPager
 import com.example.paper_slide.R
+import com.example.paper_slide.databinding.ActivityPreviewBinding
 import com.google.android.material.tabs.TabLayout
 
 class preview : AppCompatActivity() {
-    private lateinit var tabLayout: TabLayout
-    private lateinit var  viewPager: ViewPager
+    private lateinit var binding : ActivityPreviewBinding
+    private lateinit var previewViewModel: PreviewViewModel
+    private  val context = this@preview
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_preview)
+        binding = DataBindingUtil.setContentView(context,R.layout.activity_preview)
 
-        tabLayout=findViewById<TabLayout>(R.id.tablayout)
-         viewPager=findViewById<ViewPager>(R.id.viewpage)
+        previewViewModel = ViewModelProvider(context,
+            PreviewVMFactory(applicationContext)
+        )[PreviewViewModel::class.java]
+
+        val tabLayout=binding.tabLayout
+        val viewPager=binding.viewpager
 
         tabLayout.addTab(tabLayout.newTab().setText("Text"))
         tabLayout.addTab(tabLayout.newTab().setText("Image"))
@@ -38,7 +46,7 @@ class preview : AppCompatActivity() {
         }
 
 
-})
+        })
     }
 }
 
--- app/src/main/java/com/example/paper_slide/ui/result/Result.kt
+++ app/src/main/java/com/example/paper_slide/ui/result/Result.kt
@@ -21,64 +21,59 @@ class Result : AppCompatActivity() {
     private  var progressVal : Int = 10
     private lateinit var progressBar: ProgressBar
     private var id :Int  =0
-
-
-
+    private val TAG = "resultLog"
+    private lateinit var   updatedSummery : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = DataBindingUtil.setContentView(context,R.layout.activity_result)
 
         resultViewModel = ViewModelProvider(context,
             ResultVMFactory(applicationContext)
         )[ResultViewModel::class.java]
-
         seekbar()
         initViews()
-
-
     }
-
     private fun initViews() {
-        binding.resulttext.setText(intent.getStringExtra("summarizedText"))
+        //binding.resulttext.setText(intent.getStringExtra("summarizedText"))
         binding.regenerateTV.setOnClickListener {
             validateViews()
         }
         progressBar=binding.progressBar
 
+        //id = intent.getStringExtra("id")?.toInt()!!
 
-
+        binding.saveBtn.setOnClickListener {
+            validateResultET()
+        }
+    }
+    private fun validateResultET() {
+        val resultET = binding.resultET
+        updatedSummery  = resultET.text.toString()
+        if(id !=0 && updatedSummery.isNotEmpty()){
+            lifecycleScope.launch {
+                // resultViewModel.validateUpdateSummery(id,updatedSummery)
+                Toast.makeText(context, updatedSummery, Toast.LENGTH_SHORT).show()
+                Log.d(TAG, "initViews: $updatedSummery")
+            }
+        }else{
+            Toast.makeText(context, "Check id and updated summery ", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun validateViews() {
         val originalText = intent.getStringExtra("originalText")
-        val regeneratedText = binding.resulttext
-        id = intent.getStringExtra("id")?.toInt()!!
-        val updatedSummery = binding.resulttext.text.toString()
-
+        val regeneratedText = binding.resultET
         if(originalText != null) {
             lifecycleScope.launch {
-                resultViewModel.validateSummery(originalText,progressVal,binding.progressBar,regeneratedText)
+            //    resultViewModel.validateSummery(originalText,progressVal,binding.progressBar,regeneratedText)
                 // Toast.makeText(context, "$progressVal", Toast.LENGTH_SHORT).show()
                 Log.d("originalText", "validateViews: ${originalText}")
-
             }
         }else{
             Toast.makeText(context, "Provide Text", Toast.LENGTH_SHORT).show()
         }
-
-        if(id != 0 && updatedSummery != null){
-            lifecycleScope.launch {
-                resultViewModel.validateUpdateSummery(id,updatedSummery)
-            }
-
-        }else{
-
-        }
     }
-
-
     private fun seekbar() {
         binding.resultseekBar.max =10
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -88,18 +83,14 @@ class Result : AppCompatActivity() {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 progressVal = progress*10
 
-                binding.resultValue.text ="${progress * 10} /100"
+                binding.resultValue.text ="$progressVal /100"
             }
-
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
 
             }
-
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
-
                 Toast.makeText(applicationContext, "Progress: ${seekBar?.progress}", Toast.LENGTH_SHORT).show()
             }
-
-
-        })    }
+        })
+    }
 }
